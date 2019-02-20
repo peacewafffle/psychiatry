6811d90899abbd1e0d9375b135d6ced275b9c305
peacewaffle <47724745+peacewaffle@users.noreply.github.com>
2019-02-20 15:49:48 -0500

    Add files via upload

---
+++ 20190214_Rscript_Figure.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+dat = read.table("/Users/peacewaffle/Desktop/JINTEREST study A/MoCA/Analysis/20190111 MOCA N=73.csv", sep=",", header=T)
+ggplot(dat, aes(x=FTF, y=VTC, colour=Diagnosis)) + 
+  geom_point(aes(shape=Diagnosis), size=3) + 
+  scale_x_continuous(breaks = c(seq(0,30,5))) +
+  scale_y_continuous(breaks = c(seq(0,30,5))) +
+  scale_color_manual(values=c("gray22", "gray44", "gray66"))+
+  expand_limits(x=0, y=0)+
+  xlab("FTF") +
+  ylab("VTF") +
+  theme_classic()+
+  theme(aspect.ratio = 1)
+ggsave("/Users/peacewaffle/Desktop/JINTEREST study A/MoCA/Analysis/plot.png", height=5,width=5,dpi=300,units="in")
