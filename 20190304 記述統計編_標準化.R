854588769c5fcaa10bb9a52a8e27f367ef28e641
peacewaffle <47724745+peacewaffle@users.noreply.github.com>
2019-03-04 14:01:29 -0500

    Add files via upload

---
+++ 20190304 記述統計編_標準化.R
@@ -0,0 +1,28 @@
+
+x <- c(40, 46, 60, 49, 43, 60, 34, 50, 44, 33, 40, 41, 42, 47, 38)
+hist(x)
+summary(x)
+var(x)
+sd(x)
+scale(x) #標準化
+var(scale(x)) #標準化により0に
+mean(scale(x))　#標準化により1に
+hist(x, breaks = seq(20, 80, 2.5))
+
+#正規分布
+m <- mean(x)
+s <- sd(x)
+x1 <- seq(20, 80, 0.01)
+d <- dnorm(x1, mean=m, sd=s)
+plot(x1,d, type="l")
+
+#pnorm関数
+X <- 850
+m <- 582.6
+s <- 172.7
+n <- 103955
+Z <- (X-m)/s
+
+d = pnorm(Z, mean=0, sd=1) 
+n*(1-d)*100
+
